f1fcbe7d144fa1ef5ae09af988d2d20b6e5553db
ShivshankarGoit <bhola9880803447@gmail.com>
2024-06-24 16:22:24 +0530

    initial commit

---
+++ ForValidationPurpose/ForValidationPurpose/Controllers/EmployeeController.cs
@@ -0,0 +1,38 @@
+﻿using ForValidationPurpose.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ForValidationPurpose.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Employee employee)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    return RedirectToAction("Index");
+                }
+                return View();
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ ForValidationPurpose/ForValidationPurpose/Models/Employee.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ForValidationPurpose.Models
+{
+    public class Employee
+    {
+
+        public Guid EmployeeId
+        {
+            get;
+            set;
+        }
+        [Required]
+        public string FirstName
+        {
+            get;
+            set;
+        }
+        public string LastName
+        {
+            get;
+            set;
+        }
+        public int Age
+        {
+            get;
+            set;
+        }
+        public DateTime DateOfBirth
+        {
+            get;
+            set;
+        }
+        public string Address
+        {
+            get;
+            set;
+        }
+        public string Mobile
+        {
+            get;
+            set;
+        }
+
+        public string PostalCode
+        {
+            get;
+            set;
+        }
+        [RegularExpression("^[a-zA-Z0-9_\\.-]+@([a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$", ErrorMessage = "*Email is not valid.")]
+        public string EmailId
+        {
+            get;
+            set;
+        }
+
+        public string UserName
+        {
+            get;
+            set;
+        }
+        public string Password
+        {
+            get;
+            set;
+        }
+        public string ConfirmPassword
+        {
+            get;
+            set;
+        }
+        public string URL
+        {
+            get;
+            set;
+        }
+    }
+}
